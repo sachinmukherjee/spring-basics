bc078325d146ca7d83999e5c43b18f7f98315079
sachinmukherjee <sachinmukherjee29@gmail.com>
2020-02-20 22:03:31 +0530

    basic project structure

---
+++ src/main/java/sachinmukherjee/spring_basics/App.java
@@ -0,0 +1,13 @@
+package sachinmukherjee.spring_basics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
