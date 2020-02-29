eb463e286703a3bfe86b8cb646f51a3a57bc96f7
sachinmukherjee <sachinmukherjee29@gmail.com>
2020-02-29 15:39:09 +0530

    Added DI using setter injection

--- src/main/java/sachinmukherjee/spring_basics/AppDI.java
+++ src/main/java/sachinmukherjee/spring_basics/AppDI.java
@@ -10,7 +10,7 @@ public class AppDI {
 	public static void main(String[] args) {
 		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
-		Coach coach = context.getBean("cricketCoach",Coach.class);
+		Coach coach = context.getBean("hockeyCoach",Coach.class);
 		System.out.println(coach.getCoachName());
 		System.out.println(coach.getDailyTips());
 		System.out.println(coach.getDailyFortune());
